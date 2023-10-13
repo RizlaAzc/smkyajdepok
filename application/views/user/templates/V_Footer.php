c026f0782920f668bbb76b74e611816a8658b0c9
siskasuri <najemlijen@gmail.com>
2023-10-13 14:07:53 +0700

    js

--- application/views/user/templates/V_Footer.php
+++ application/views/user/templates/V_Footer.php
@@ -52,12 +52,12 @@
               <div class="footer-head">
                 <h4>Instagram</h4>
                 <div class="flicker-img">
-                  <a href="#"><img src="<?= base_url('assets/');?>img/portfolio/1.jpg" alt=""></a>
-                  <a href="#"><img src="<?= base_url('assets/');?>img/portfolio/2.jpg" alt=""></a>
-                  <a href="#"><img src="<?= base_url('assets/');?>img/portfolio/3.jpg" alt=""></a>
-                  <a href="#"><img src="<?= base_url('assets/');?>img/portfolio/4.jpg" alt=""></a>
-                  <a href="#"><img src="<?= base_url('assets/');?>img/portfolio/5.jpg" alt=""></a>
-                  <a href="#"><img src="<?= base_url('assets/');?>img/portfolio/6.jpg" alt=""></a>
+                  <a href="#"><img src="<?= base_url('assets/user/');?>img/portfolio/1.jpg" alt=""></a>
+                  <a href="#"><img src="<?= base_url('assets/user/');?>img/portfolio/2.jpg" alt=""></a>
+                  <a href="#"><img src="<?= base_url('assets/user/');?>img/portfolio/3.jpg" alt=""></a>
+                  <a href="#"><img src="<?= base_url('assets/user/');?>img/portfolio/4.jpg" alt=""></a>
+                  <a href="#"><img src="<?= base_url('assets/user/');?>img/portfolio/5.jpg" alt=""></a>
+                  <a href="#"><img src="<?= base_url('assets/user/');?>img/portfolio/6.jpg" alt=""></a>
                 </div>
               </div>
             </div>
@@ -93,14 +93,14 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="<?= base_url('assets/');?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<?= base_url('assets/');?>vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="<?= base_url('assets/');?>vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="<?= base_url('assets/');?>vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="<?= base_url('assets/');?>vendor/php-email-form/validate.js"></script>
+  <script src="<?= base_url('assets/user/');?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url('assets/user/');?>vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?= base_url('assets/user/');?>vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?= base_url('assets/user/');?>vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?= base_url('assets/user/');?>vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="<?= base_url('assets/');?>js/main.js"></script>
+  <script src="<?= base_url('assets/user/');?>js/main.js"></script>
 
 </body>
 
--- application/views/user/templates/V_Head.php
+++ application/views/user/templates/V_Head.php
@@ -10,22 +10,22 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="<?= base_url('assets/');?>img/smk.png" rel="icon">
-  <link href="<?= base_url('assets/');?>img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?= base_url('assets/user/');?>img/smk.png" rel="icon">
+  <link href="<?= base_url('assets/user/');?>img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?= base_url('assets/');?>vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="<?= base_url('assets/');?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?= base_url('assets/');?>vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?= base_url('assets/');?>vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?= base_url('assets/');?>vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="<?= base_url('assets/');?>vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/user/');?>vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/user/');?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/user/');?>vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= base_url('assets/user/');?>vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/user/');?>vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/user/');?>vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="<?= base_url('assets/');?>css/style.css" rel="stylesheet">
+  <link href="<?= base_url('assets/user/');?>css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: eBusiness
