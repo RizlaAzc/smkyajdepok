6c023153c1e4313b8e2250484f814bc8432c08e4
RizlaAzc <azchafahrezi@gmail.com>
2023-10-13 13:30:02 +0700

    a

--- application/views/user/templates/V_Head.php
+++ application/views/user/templates/V_Head.php
@@ -5,27 +5,27 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>eBusiness Bootstrap Template - Index</title>
+  <title><?= $title; ?></title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?= base_url('assets/');?>img/smk.png" rel="icon">
+  <link href="<?= base_url('assets/');?>img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/');?>vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/');?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/');?>vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= base_url('assets/');?>vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/');?>vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/');?>vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="<?= base_url('assets/');?>css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: eBusiness
