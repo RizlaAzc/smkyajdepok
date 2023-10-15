3877353bd7a2fc3420f102e70d39ab962bb85b8a
ozaky96 <sitisuryanilubis06@gmail.com>
2023-10-15 16:48:57 +0700

    m

--- application/views/user/pages/ppdb/V_Ppdb.php
+++ application/views/user/pages/ppdb/V_Ppdb.php
@@ -20,84 +20,380 @@
       </div>
     </div>  
     
-    <div class="row">
-    <div class="col-12">
-        <div class="card">
-            <form action="<?= base_url('karyawan/store') ?>" method="post">
-                <div class="card-header">
-                    <h4 class="card-title">Lengkapi formulir Berikut</h4>
-                </div>
+  <main id="main">
 
-                <div class="card-body border-top py-0 my-3  ">
-                    <h4 class="text-muted my-4">Data Pendaftaran</h4> 
-                    
+    <!-- ======= Blog Page ======= -->
+    <div class="blog-page area-padding">
+      <div class="container">
+        <div class="row">
+          
+          <!-- Start  contact -->
+          <div class="col-md-6">
+              <div class="form contact-form">
+              <h4 class="text-muted my-3">Data Pendaftaran</h4>
+                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+
+                  <div class="form-group">
                     <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-4">
-                            <div class="form-group">
-                                <label>Jalur Pendaftaran : </label>
-                                <select name="Jalur_pendaftaran" id="Jalur_pendaftaran" class="form-control">
-                                    <option value="" disabled selected>-- Choose --</option>
-                                    <option value="1">Online</option>
-                                </select>
-                            </div>
-                        </div>
-                      
+                    <div class="col-sm-3 mt-2">
+                        <p>Jalur Pendaftaran</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <select name="jalurpendaftaran" class="form-control" id="jalurpendaftaran"  required>
+                        <option value="" disabled selected> </option>
+                        <option value="1">Online</option>
+                        </select>
+                    </div>
+                    </div>
+                  </div>
 
-                        <div class="col-xs-12 col-sm-12 col-md-4">
-                            <div class="form-group">
-                                <label for="divisi">Pilihan I : </label>
-                                <select name="Jalur_pendaftaran" id="Jalur_pendaftaran" class="form-control">
-                                    <option value="" disabled selected>-- Choose --</option>
-                                    <option value="1">Teknik Jaringan Komputer dan Telekomunikasi</option>
-                                    <option value="2">Pengembangan Perangkat Lunak dan Gim</option>
-                                    <option value="3">Akuntansi Keuangan Lembaga</option>
-                                    <option value="4">Manajemen Perkantoran Lembaga Binis</option>
-                                </select>
-                            </div>
-                        </div>
-                    
-                      
-                        <div class="col-xs-12 col-sm-12 col-md-4">
-                            <div class="form-group">
-                                <label for="divisi">Pilihan II : </label>
-                                <select name="Jalur_pendaftaran" id="Jalur_pendaftaran" class="form-control">
-                                    <option value="" disabled selected>-- Choose --</option>
-                                    <option value="1">Teknik Jaringan Komputer dan Telekomunikasi</option>
-                                    <option value="2">Pengembangan Perangkat Lunak dan Gim</option>
-                                    <option value="3">Akuntansi Keuangan Lembaga</option>
-                                    <option value="4">Manajemen Perkantoran Lembaga Binis</option>
-                                </select>
-                            </div>
-                        </div>
-                                   
-                     </div>
-                     <br>
-                    
-                     <div class="row">
-                        <div class="col-xs-12 col-sm-6 col-md-4">
-                            <div class="form-group">
-                                <label for="sekolahasal">Sekolah Asal : </label>
-                                <input type="text" name="Sekolah_Asal" id="sekolahasal" class="form-control" placeholder="Masukan Asal Sekolah Siswa" required="reuqired" />
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-6 col-md-4">
-                            <div class="form-group">
-                                <label for="nisn">NISN : </label>
-                                <input type="text" name="nisn" id="nisn" class="form-control" placeholder="Masukan NISN Siswa" required="reuqired" />
-                            </div>
-                        </div>
-                    </div> 
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Pilihan I</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <select name="jalurpendaftaran" class="form-control" id="jalurpendaftaran"  required>
+                        <option value="" disabled selected> </option>
+                        <option value="1">Teknik Jaringan Komputer dan Telekomunikasi</option>
+                        <option value="2">Pengembangan Perangkat Lunak dan Gim</option>
+                        <option value="3">Akuntansi Keuangan Lembaga</option>
+                        <option value="4">Manajemen Perkantoran Lembaga Binis</option>
+                        </select>
+                    </div>
+                    </div>
+                  </div>
 
-                </div>
-            
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Pilihan II</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <select name="jalurpendaftaran" class="form-control" id="jalurpendaftaran"  required>
+                        <option value="" disabled selected> </option>
+                        <option value="1">Teknik Jaringan Komputer dan Telekomunikasi</option>
+                        <option value="2">Pengembangan Perangkat Lunak dan Gim</option>
+                        <option value="3">Akuntansi Keuangan Lembaga</option>
+                        <option value="4">Manajemen Perkantoran Lembaga Binis</option>
+                        </select>
+                    </div>
+                    </div>
+                  </div>
+                  
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Asal Sekolah</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="asalsekolah" id="asalsekolah" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
 
-
-               
-                <!-- <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Simpan <i class="fa fa-save"></i></button>
-                </div> -->
-            </form>
-        </div>
-    </div>
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>NISN</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="nisn" id="nisn" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
 </div>
-<br>
+</div>
+
+
+
+<!-- <div class="row"> -->
+            <div class="col-md-6">
+              <div class="form contact-form">
+              <h4 class="text-muted my-3">Data Calon Peserta Didik Baru</h4>
+
+              <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Nama Lengkap</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="namalengkap" id="namalengkap" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Tempat Lahir</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="tempatlahir" id="tempatlahir" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Tanggal Lahir</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="date" class="form-control" name="tanggallahir" id="tanggallahir" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Jenis Kelamin</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <select name="jeniskelamin" class="form-control" id="jeniskelamin"  required>
+                        <option value="" disabled selected> </option>
+                        <option value="1">Laki - Laki</option>
+                        <option value="2">Perempuan</option>
+                        </select>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Agama</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <select name="agama" class="form-control" id="agama"  required>
+                        <option value="" disabled selected> </option>
+                        <option value="1">Islam</option>
+                        <option value="2">Kristen</option>
+                        <option value="3">Katholik</option>
+                        <option value="4">Hindu</option>
+                        <option value="5">Budha</option>
+                        <option value="6">Konghucu</option>
+                        </select>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Status Anak</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <select name="statusanak" class="form-control" id="statusanak"  required>
+                        <option value="" disabled selected> </option>
+                        <option value="1">Anak Kandung</option>
+                        <option value="2">Anak Angkat</option>
+                        </select>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Anak Ke</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="anakke" id="anakke" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Alamat</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <textarea class="form-control" name="alamat" rows="4" placeholder=" " required></textarea>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Telephone</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="telephone" id="telephone" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>TK Asal</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="tkasal" id="tkasal" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Email</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="email" class="form-control" name="email" id="email" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Photo</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="file" class="form-control" name="file" id="file" required>
+                        <div class="block-error" style="color:#333;">Tipe file harus JPG dan ukuran maksimal photo 1 MB</div>
+                    </div>
+                    </div>
+                  </div>
+</div></div>
+
+                  
+       <!-- <div class="row"> -->
+            <div class="col-md-6">
+              <div class="form contact-form">
+              <h4 class="text-muted my-3">Data Orang Tua / Wali</h4>
+
+              <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Nama Ayah</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="namaayah" id="namaayah" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Nama Ibu</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="namaibu" id="namaibu" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Alamat Orang Tua</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <textarea class="form-control" name="alamatortu " rows="4" placeholder=" " required></textarea>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Telephone Orang Tua</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="telephoneortu" id="telephoneortu" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Pekerjaan Ayah</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="pekerjaanayah" id="pekerjaanayah" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+</div>
+</div>
+<div class="col-md-6" style="margin-top: 60px;">
+    <div class="form contact-form">
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Pekerjaan Ibu</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="pekerjaanibu" id="pekerjaanibu" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Nama wali</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="namawali" id="namawali" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Alamat Wali</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <textarea class="form-control" name="alamatwali " rows="4" placeholder=" " required></textarea>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                    <div class="col-sm-3 mt-2">
+                        <p>Telephone Wali</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <input type="text" class="form-control" name="telephonewali" id="telephonewali" placeholder=" " required>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="row">
+                        <div class="col-sm-3 mt-2">
+                        <p>Pekerjaan Wali</p>
+                        </div>
+                        <div class="col-sm-9">
+                        <input type="text" class="form-control" name="pekerjaanwali" id="pekerjaanwali" placeholder=" " required>
+                        </div>
+                    </div>
+                  </div>
+</div></div>
+
+              
+
+
+                    <div class="text-center"><br><button class="btn btn-primary" type="submit">Simpan Formulir Pendaftaran</button></div>
+                </form>
+              </div>
+            </div>
+            <!-- End Left contact -->
+          </div>
+        </div>
+      </div>
+    </div><!-- End Blog Page -->
+
+  </main><!-- End #main -->
