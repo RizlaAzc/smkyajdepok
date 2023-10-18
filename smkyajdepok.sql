d4097a8275ddd67cd9790c97d86929484aa26c48
RizlaAzc <azchafahrezi@gmail.com>
2023-10-18 13:23:45 +0700

    a

--- smkyajdepok.sql
+++ smkyajdepok.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2023 at 11:23 AM
+-- Generation Time: Oct 18, 2023 at 08:22 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -24,16 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `carousel`
+--
+
+CREATE TABLE `carousel` (
+  `id` int(11) NOT NULL,
+  `subjek` varchar(255) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `carousel`
+--
+
+INSERT INTO `carousel` (`id`, `subjek`, `deskripsi`, `gambar`) VALUES
+(1, 'tes', 'tes', ''),
+(2, 'asdasd', 'asdasd', ''),
+(3, 'aisjd', 'oljasd', ''),
+(4, 'hehe', 'hehe', 'profil.png');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login`
 --
 
 CREATE TABLE `login` (
   `id` int(11) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `foto` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `telepon` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `date_created` date NOT NULL,
   `is_active` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,8 +66,10 @@ CREATE TABLE `login` (
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`id`, `nama`, `email`, `foto`, `password`, `date_created`, `is_active`) VALUES
-(1, 'admin', 'admin@gmail.com', 'profil.png', '$2y$10$HOUwLLOU.5SudZZtmlVPtOIZ4BOAfwIMPxYIbLBuHwMfoT3ckLoMe', '2023-10-13 09:12:53', 1);
+INSERT INTO `login` (`id`, `nama_lengkap`, `nama`, `email`, `password`, `telepon`, `foto`, `date_created`, `is_active`) VALUES
+(1, 'admin cms', 'admin', 'admin@gmail.com', '$2y$10$awxE5A4xvS0q52GzajvqOOYqSfaF1L7cfUW.AG1mU/cXMrtiSC.zy', '000000000', 'profil1.png', '2023-10-14', 1),
+(2, 'zc', 'asd', 'rinisaja@gmail.com', '$2y$10$S2Qs.MjVqTjSTMNAi7dI3eYj3dSkB8NIN47tUYjPLVT01dTwbQdY6', '\\', 'profil.png', '2023-10-15', 1),
+(3, 'zc', 'admin', 'a@asd.com', '$2y$10$XOnoqUjhneWb7Fd0P2fiH.b1HunhPrfabZ2rJW1AObZgkMo0eqNiK', '123\\', 'profil.png', '2023-10-15', 1);
 
 -- --------------------------------------------------------
 
@@ -76,6 +103,12 @@ CREATE TABLE `sosial_media` (
 --
 
 --
+-- Indexes for table `carousel`
+--
+ALTER TABLE `carousel`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
@@ -98,10 +131,16 @@ ALTER TABLE `sosial_media`
 --
 
 --
+-- AUTO_INCREMENT for table `carousel`
+--
+ALTER TABLE `carousel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pesan`
