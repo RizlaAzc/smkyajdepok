5d3d4193714e5a7e317b45ac462622ba4fd5a5f1
ozaky96 <sitisuryanilubis06@gmail.com>
2023-10-21 14:57:40 +0700

    x

--- application/controllers/ppdb.php
+++ application/controllers/ppdb.php
@@ -7,6 +7,7 @@ class ppdb extends CI_Controller {
 	{
 		parent:: __construct();
 		$this->load->model('model_study');
+		$this->load->model('model_ppdb');
 	}
 
 	/**
@@ -27,11 +28,91 @@ class ppdb extends CI_Controller {
 	public function index()
 	{
 		$data['study'] = $this->model_study->getDataStudy();
-
-		$this->load->view('user/templates/V_Head');
+		
+		$title['title'] = " PPDB SMK YAJ";
+		$this->load->view('user/templates/V_Head', $title);
 		$this->load->view('user/templates/V_Navbar');
 		$this->load->view('user/pages/ppdb/V_Ppdb', $data);
 		$this->load->view('user/templates/V_Footer');
 	}
 
+	public function ppdb()
+    {
+        $jalurpendaftaran = $this->input->post('jalurpendaftaran');
+        $pilihan1 = $this->input->post('pilihan1');
+		$pilihan2 = $this->input->post('pilihan2');
+        $asalsekolah = $this->input->post('asalsekolah');
+        $nisn = $this->input->post('nisn');
+		$namalengkap = $this->input->post('namalengkap');
+		$tempatlahir = $this->input->post('tempatlahir');
+		$tanggallahir = $this->input->post('tanggallahir');
+		$jeniskelamin = $this->input->post('jeniskelamin');
+		$agama = $this->input->post('agama');
+		$statusanak = $this->input->post('statusanak');
+		$anakke = $this->input->post('anakke');
+		$alamat = $this->input->post('alamat');
+		$telephone = $this->input->post('telephone');
+		$email = $this->input->post('email');
+		$photo = $_FILES['photo'];
+
+        if ($photo = '') {
+        } else {
+            $config['upload_path'] = 'assets/user/img/ppdb';
+            $config['allowed_types'] = 'jpg';
+
+            $this->load->library('upload');
+            $this->upload->initialize($config);
+            if (!$this->upload->do_upload('photo')) {
+                echo "Upload Gagal";
+            } else {
+                $photo = $this->upload->data('file_name');
+            }
+        }
+
+		$namaayah = $this->input->post('namaayah');
+		$namaibu = $this->input->post('namaibu');
+		$alamatortu = $this->input->post('alamatortu');	
+		$telephoneortu = $this->input->post('telephoneortu');
+		$pekerjaanayah = $this->input->post('pekerjaanayah');
+		$pekerjaanibu = $this->input->post('pekerjaanibu');
+		$namawali = $this->input->post('namawali');
+		$alamatwali = $this->input->post('alamatwali');
+		$telephonewali = $this->input->post('telephonewali');
+		$pekerjaanwali = $this->input->post('pekerjaanwali');
+
+
+        $ArrInsert = array(
+            'jalurpendaftaran' => $jalurpendaftaran,
+			'pilihan1' => $pilihan1,
+			'pilihan2' => $pilihan2,
+			'asalsekolah' => $asalsekolah,
+			'nisn' => $nisn,
+			'namalengkap' => $namalengkap,
+			'tempatlahir' => $tempatlahir,
+			'tanggallahir' => $tanggallahir,
+			'jeniskelamin' => $jeniskelamin,
+			'agama' => $agama,
+			'statusanak' => $statusanak,
+			'anakke' => $anakke,
+			'alamat' => $alamat,
+			'telephone' => $telephone,
+            'email' => $email,
+			'photo' => $photo,
+            'namaayah' => $namaayah,
+            'namaibu' => $namaibu,
+			'alamatortu' => $alamatortu,
+			'telephoneortu' => $telephoneortu,
+			'pekerjaanayah' => $pekerjaanayah,
+			'pekerjaanibu' => $pekerjaanibu,
+			'namawali' => $namawali,
+			'alamatwali' => $alamatwali,
+			'telephonewali' => $telephonewali,
+			'pekerjaanwali' => $pekerjaanwali
+            
+        );
+
+        $this->model_ppdb->insertDataPpdb($ArrInsert);
+        $this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">Formulir Berhasil di Simpan!</div>');
+        redirect(base_url('ppdb'));
+    }
 }
